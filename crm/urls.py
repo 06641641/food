66f9ef72e184ce0c52b5d1c5e1040f15c5efa80c
Nick <rpruitt@unomaha.edu>
2020-04-15 22:56:05 -0500

    2P2 Upload

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path, re_path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'crm'
 urlpatterns = [
@@ -26,4 +27,8 @@ urlpatterns = [
 
     #Summary
     path('customer/<int:pk>/summary/', views.summary, name='summary'),
+
+    url(r'^customers_json/', views.CustomerList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
